904223f49dd4808d294d45c3ab98b3b85993d08c
derelines <113526273+derelines@users.noreply.github.com>
2023-05-12 09:01:32 +0300

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,20 @@
+    //функция для смены изображения при наведении мыши
+    function setnewimage() {
+    document.getElementById("img1").src = "pics/Group 4.svg";
+    }
+    function setnewimage1() {
+    document.getElementById("img2").src = "pics/Group 5.svg";
+    }
+    function setnewimage2() {
+    document.getElementById("img3").src = "pics/Group 6.svg";
+    }
+    //функция для смены изображения на начальное при убирании курсора с изображения
+    function setoldimage1() {
+    document.getElementById("img1").src = "pics/Group 1.svg";
+    }
+    function setoldimage2() {
+    document.getElementById("img2").src = "pics/Group 2.svg";
+    }
+    function setoldimage3() {
+    document.getElementById("img3").src = "pics/Group 3.svg";
+    }
